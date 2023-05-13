09cb81bce94a82783280abd3b026e24f6f572200
june0216 <ziyun1612@ewhain.net>
2023-05-14 01:14:07 +0900

    feat : 댓글 api 구현

---
+++ src/main/java/com/efub/community/domain/board/controller/BoardPostController.java
@@ -0,0 +1,34 @@
+package com.efub.community.domain.board.controller;
+
+import com.efub.community.domain.board.domain.Post;
+import com.efub.community.domain.board.dto.response.PostListResponseDto;
+import com.efub.community.domain.board.dto.request.PostRequestDto;
+import com.efub.community.domain.board.dto.response.PostResponseDto;
+import com.efub.community.domain.board.service.PostService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/boards")
+@RequiredArgsConstructor
+public class BoardPostController {
+	private final PostService postService;
+
+	@GetMapping("/{boardId}/posts")
+	public PostListResponseDto readPostList(@PathVariable final Long boardId) {
+		List<Post> postList = postService.findPostsByBoard(boardId);
+		return PostListResponseDto.of(postList);
+	}
+
+	@PostMapping("/{boardId}/posts")
+	public PostResponseDto createPost(@PathVariable final Long boardId, @RequestBody @Valid final PostRequestDto requestDto) {
+		Long id = postService.create(boardId, requestDto);
+		Post post = postService.findById(id);
+		return PostResponseDto.of(post);
+	}
+}
---
+++ src/main/java/com/efub/community/domain/member/controller/MemberController.java
@@ -0,0 +1,61 @@
+package com.efub.community.domain.member.controller;
+
+import com.efub.community.domain.member.domain.Member;
+import com.efub.community.domain.member.dto.*;
+import com.efub.community.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
+public class MemberController {
+	private final MemberService memberService;
+
+	@PostMapping
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public MemberResponseDto signUp(@RequestBody @Valid final SignUpRequestDto requestDto) {
+		Long id = memberService.signUp(requestDto);
+		Member findMember = memberService.findById(id);
+		return new MemberResponseDto(findMember);
+	}
+
+	@PostMapping("/login")
+	@ResponseStatus(value = HttpStatus.OK)
+	public LoginResponseDto login(@RequestBody final LoginRequestDto requestDto)
+	{
+		Long memberId = memberService.login(requestDto);
+		return new LoginResponseDto(memberId);
+	}
+	@GetMapping("/{memberId}")
+	@ResponseStatus(value = HttpStatus.OK)
+	public MemberResponseDto getMember(@PathVariable Long memberId)
+	{
+		Member findMember = memberService.findById(memberId);
+		return new MemberResponseDto(findMember);
+	}
+
+
+	@PatchMapping("/profile/{memberId}")
+	@ResponseStatus(value = HttpStatus.OK)
+	public MemberResponseDto update(@PathVariable final Long memberId, @RequestBody @Valid final MemberUpdateRequestDto requestDto) {
+		Long id = memberService.update(memberId,requestDto);
+		Member findMember = memberService.findById(memberId);
+		return new MemberResponseDto(findMember);
+	}
+
+	@PatchMapping("/{memberId}")
+	@ResponseStatus(value = HttpStatus.OK)
+	public String withdraw(@PathVariable long memberId)
+	{
+		memberService.withdraw(memberId);
+		return "성공적으로 탈퇴가 완료되었습니다";
+	}
+
+}
